ad32d3e6481d8c6a96b507499cff9e2c8a9ac541
Hazim Hanif <hazimhanif0@gmail.com>
2016-05-03 23:33:45 +0800

    simplified based on rankhospital.

--- R-Programming/Week-4/best.R
+++ R-Programming/Week-4/best.R
@@ -10,77 +10,33 @@ best <- function(state, outcome) {
   
   #check the outcome if it exist or not#
   if(outcome=="heart attack"){
-        
-        data[, 11] <- as.numeric(data[, 11])
-        data[, 2] <- as.character(data[, 2])
-        combine <- data.frame(data$State,data$Hospital.Name,data[, 11])
-        combine <- na.omit(combine)
-        combine <- combine[which(combine$data.State==as.character(state)),1:3]
-        
-        order.pop <- order(combine$data...11.)
-        combine <- combine[order.pop,]
-        
-        ties <- combine[which(combine$data...11.==combine[1,3]),1:3]
-        
-        if(nrow(ties)>1){
-          
-          order2.pop <- order(ties$data.Hospital.Name)
-          ties <- ties[order2.pop,]
-          return(as.vector(as.character(ties[1,2])))
-          
-        }else{
-          return(as.vector(as.character(combine[1,2])))
-        }
-        
-  }else if(outcome=="heart failure"){
-    
-        data[, 17] <- as.numeric(data[, 17])
-        data[, 2] <- as.character(data[, 2])
-        combine <- data.frame(data$State,data$Hospital.Name,data[, 17])
-        combine <- na.omit(combine)
-        combine <- combine[which(combine$data.State==as.character(state)),1:3]
-        
-        order.pop <- order(combine$data...17.)
-        combine <- combine[order.pop,]
-        
-        ties <- combine[which(combine$data...17.==combine[1,3]),1:3]
-        
-        if(nrow(ties)>1){
-          
-          order2.pop <- order(ties$data.Hospital.Name)
-          ties <- ties[order2.pop,]
-          return(as.vector(as.character(ties[1,2])))
-          
-        }else{
-          return(as.vector(as.character(combine[1,2])))
-        }
-    
-  }else if(outcome=="pneumonia"){
-        
-        data[, 23] <- as.numeric(data[, 23])
-        data[, 2] <- as.character(data[, 2])
-        combine <- data.frame(data$State,data$Hospital.Name,data[, 23])
-        combine <- na.omit(combine)
-        combine <- combine[which(combine$data.State==as.character(state)),1:3]
-        
-        order.pop <- order(combine$data...23.)
-        combine <- combine[order.pop,]
-        
-        ties <- combine[which(combine$data...23.==combine[1,3]),1:3]
-        
-        if(nrow(ties)>1){
-          
-          order2.pop <- order(ties$data.Hospital.Name)
-          ties <- ties[order2.pop,]
-          return(as.vector(as.character(ties[1,2])))
-          
-        }else{
-          return(as.vector(as.character(combine[1,2])))
-        }
-        
-  }else{
-    stop("invalid outcome")
+    colnum <- 11
+   }else if(outcome=="heart failure"){
+     colnum <- 17
+   }else if(outcome=="pneumonia"){
+    colnum <- 23
+   }else{
+     stop("invalid outcome")
   }
+  
+  data[, colnum] <- as.numeric(data[, colnum])
+  data[, 2] <- as.character(data[, 2])
+  combine <- data.frame(data$State,data$Hospital.Name,data[, colnum])
+  combine <- na.omit(combine)
+  combine <- combine[which(combine$data.State==as.character(state)),1:3]
+  
+  order.pop <- order(combine[,3])
+  combine <- combine[order.pop,] 
+  
+  ties <- combine[which(combine[,3]==combine[1,3]),1:3]
+  
+  if(nrow(ties)>1){
+   order2.pop <- order(ties$data.Hospital.Name)
+   ties <- ties[order2.pop,]
+   return(as.vector(as.character(ties[1,2])))
+  }else{
+    return(as.vector(as.character(combine[1,2])))
+  }  
   
 }
 
